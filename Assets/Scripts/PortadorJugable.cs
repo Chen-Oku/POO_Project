b1250013b2253ae116b1bc463c710b0a284a007d
Chen-Oku <chenoku@gmail.com>
2025-04-28 15:32:36 -0500

    123

--- Assets/Scripts/PortadorJugable.cs
+++ Assets/Scripts/PortadorJugable.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
 
-public class PortadorJugable
+public class PortadorJugable : PortadorGeneral
 {
-    
+    private SistemaVida sistemaVida;
+    private SistemaMana sistemaMana;
+        public PortadorJugable(float vidaActual, float vidaMaxima, float manaActual, float manaMaximo) : base(vidaActual, vidaMaxima)
+        {
+            this.sistemaVida = new SistemaVida(vidaActual, vidaMaxima);
+            this.sistemaMana = new SistemaMana(manaActual, manaMaximo);
+        }
+  
+        public void Curar(float cantidad)
+        {
+            sistemaVida.curar(cantidad);
+        }
+        public void ConsumirMana(float cantidad)
+        {
+            sistemaMana.consumirMana(cantidad);
+        }
+        public void RegenerarMana(float cantidad)
+        {
+            sistemaMana.regenerarMana(cantidad);
+        }
 }
+
